5e4162530741cb4da39bda47bb779f3250119952
Lucemy Ferreira <lucemyjunior@hotmail.com>
2024-01-30 20:43:37 -0300

    Adding Routes to the Navbar buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import {Route, Routes,} from "react-router-dom"
 import Navbar from './Components/Navbar/Navbar';
-import {Routes} from "react-router-dom"
+import Home from './Components/Routes/Home';
+import Contact from './Components/Routes/Contact';
+import Service from './Components/Routes/Service';
+import About from './Components/Routes/About';
 
 function App() {
   return (
     <div className="App">
-        <Navbar/>
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/about" element = {<About/>}/>
+        <Route path = "/service" element = {<Service/>}/>
+        <Route path = "/contact" element = {<Contact/>}/>
+      </Routes>
+
+      <Navbar/>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -21,10 +21,10 @@ class Navbar extends Component {
                 <ul className={this.state.clicked ? "nav-menu active" : "nav-menu "}>
                 {MenuItens.map((item, index) => (
                     <li key={index}>
-                    <a className={item.cName} href={item.url}>
+                    <Link className={item.cName} to={item.url}>
                         <i className={item.icon}>
                         </i>{item.title}
-                    </a>
+                    </Link>
                     </li>
                 ))}
                 <button>Sign Up</button>
